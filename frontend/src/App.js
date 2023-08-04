24d1e3fe7d41b394830e8015b1f74cccac37ddfe
shubhamchepe <hemantchepe@gmail.com>
2023-08-04 09:22:36 +0530

    32th commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,9 +9,28 @@ import push from "push.js";
 
 
 function App() {
+  // const userPermission = async () => {
+  //   const permission = await window.Notification.requestPermission();
+  //   if (permission === "granted") {
+  //     const token = await getToken(messaging, {
+  //       vapidKey: process.env.WEB_PUSH_CERTIFICATE,
+  //     });
+  //     console.log(token);
+  //   } else if (permission === "denied") {
+  //     alert("Notofication Denied!");
+  //   }
+  // };
+  const onGranted = () => {
+    console.log("granted");
+  }
 
+  const onDenied = () => {
+    console.log("denied");
+  }
 
-
+  useEffect(() => {
+    push.Permission.request(onGranted, onDenied);
+  }, []);
 
   return (
     <div className="App">
@@ -22,6 +41,23 @@ function App() {
   );
 }
 
+function urlBase64ToUint8Array(base64String) {
+  try {
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, "+")
+      .replace(/_/g, "/");
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
 
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  } catch (error) {
+    console.error("Error converting VAPID public key to Uint8Array:", error);
+    return null;
+  }
+}
 
 export default App;
